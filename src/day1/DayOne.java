f6f731b1cb4a5261e4a1229502c73ffaa8f3632e
Urgyen Shakya <urgyenshakya@gmail.com>
2020-04-02 09:50:05 +0545

    day2 cmmited

---
+++ src/day1/DayOne.java
@@ -0,0 +1,89 @@
+package day1;
+
+//public --> access modifier; public;
+//class --> 
+//class --> capital Letter;
+
+public class DayOne {
+//static-- > later;
+	//void --> return type
+		//main --> method 
+	public static void main (String[] args) {
+
+		System.out.println("Hello World");
+		
+		//data type??
+		//variables?? --> vary huna sakcha?
+		
+		
+		//String age = "Urgyen";
+		
+		//variables?
+		//naming convention?  --> WArning  :: ERROR
+		//Class name-- > Capital :: Noun 
+		int age= 24;
+	
+		// not a good practise;
+		
+	//	int x=77;
+	//	int y=12;
+		
+		//lowerCamelCasing
+		String mynameis= "Urgyen";
+		
+		//Datatype;?
+		
+				
+				//java double use garxu
+				
+		//int, float, double. long
+		//byte, boolean
+		//String, char
+		
+		//Operator?
+				//+ - * / %
+		
+		int num1= 15;
+		int num2=  7;
+		
+		int sum= num1%num2;
+		
+		System.out.println(sum);
+		
+		//Assignments operators;
+		int age1 = 28;
+		//30;
+		
+		//int age2= age1 + 18;
+		
+		age1++;
+		age1++;
+		
+		
+		System.out.println(age1);
+		
+		
+		//greater, lower
+		// equals or not equals
+		
+		//ternary operator?
+		
+		//if else;
+	//	  2 number? minimum
+				  
+		  int x=15;
+		  int y=70;
+		  
+		int minimumNumber= ( x > y ) ? y : x ; 
+		System.out.println("Minimum Number: ");
+		System.out.println(minimumNumber);
+				  
+		
+		
+	}
+	
+}
+
+
+//task1 --> detailed anatomy 
+
---
+++ src/day2/CtrlStatements.java
@@ -0,0 +1,149 @@
+package day2;
+
+public class CtrlStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// direct statemnsts;
+
+		// Control Statements?
+		// do again and again
+		// Looping
+		// for // do while
+
+		// condition check
+		// branching
+		// if else , else if , if
+		// switch condition
+
+//		int age = 18;
+//		double height =5.6;
+//				
+//				//5.5> age 19- --> Hero in the college;
+//				
+//		// if  or if else
+//		
+//		//And opertor?
+//		
+//		//or Operator?
+//		
+//		if(age<20 && height > 5.5) {
+//			System.out.println("Hero in College ");
+//		}
+//		
+
+		
+		
+		// else if;
+		// Age Group
+//		20-			-> Teen
+//		20+ 40-- 	--> Adult
+//		40 +		-->Old
+		// top to down approach;
+
+//		int age1= 25;
+//		if(age1<20) {
+//			System.out.println("Teen");
+//		}
+//		else if(age1<40){
+//			System.out.println("Adult");
+//		}
+//		else {
+//			System.out.println("Old");
+//		}
+
+		// switch condition?
+//		int weekDay = 10;
+//		switch (weekDay) {
+//		case 1:
+//			System.out.println("Sunday");
+//			break;
+//
+//		case 2:
+//			System.out.println("Monday");
+//			break;
+//
+//		case 3:
+//			System.out.println("Tuesday");
+//			break;
+//			
+//		case 4:
+//			System.out.println("Wedday");
+//			break;
+//			
+//		case 5:
+//			System.out.println("Thursday");
+//			break;
+//			
+//		case 6:
+//			System.out.println("Friday");
+//			break;
+//			
+//		case 7:
+//			System.out.println("Saturday");
+//			break;
+//			
+//		default:
+//			System.out.println("Grow Up");
+//			
+//		} //eta pugna parne
+		
+		//looping
+		//Ctrl + /
+		
+		//index
+//		int loopFor=5;
+//		
+//		while(loopFor==7) {
+//			System.out.println("Mero naamm");
+//			System.out.println(loopFor);
+//
+//			//loopFor= loopFor - 1;
+//			//Auto Decrement;
+//			//checker
+//			loopFor++;
+//		}
+//		
+		//do while??
+		//self?
+		
+		//for
+		//1+2+3+4+5+6+7+8+9+10
+		//1*2*3*4*5*6*7*8*9*10
+		
+//		int sum=0;
+//		for( int index=1; index<=10 ;index++) {
+//			sum=sum+index;
+//			System.out.println("Per Line: " +sum);
+//
+//		}
+//	
+//		System.out.println("------");
+//		System.out.println("Totsl sum " + sum);
+
+//		int fact=1;
+//		for( int index=1; index<=10 ;index++) {
+//			fact=fact*index;
+//			System.out.println("Per Line: " +fact);
+//		}
+//	
+//		System.out.println("------");
+//		System.out.println("Fctorial " + fact);
+//		
+//		
+		
+		String name="Rajesh";
+		
+		if(name=="Rajesh") {
+			System.out.println("My name is Rajesh");
+		}	
+		
+		
+		
+		
+		
+
+	}
+
+}
